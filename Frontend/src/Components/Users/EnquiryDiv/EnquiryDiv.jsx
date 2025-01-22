12a9ae72af51d8033d02a1c4aaafb33f9b4a545f
shijiaa <shijihcl@gmail.com>
2025-01-22 16:50:38 +0530

    FrontEnd

--- Frontend/src/Components/Users/EnquiryDiv/EnquiryDiv.jsx
+++ Frontend/src/Components/Users/EnquiryDiv/EnquiryDiv.jsx
@@ -6,9 +6,15 @@ function EnquiryDiv({ bgColor = 'bg-navy', textColor = 'text-white', buttonBgCol
                     <h1 className="text-3xl font-semibold">Ready to make an Impact?</h1>
                     <p className="text-lg mt-2">Join the CADD Centre family and lead the way in skill training.</p>
                 </div>
-                <button className={`${buttonBgColor} ${buttonTextColor} py-2 px-6 rounded-lg font-semibold transition-all duration-300 ease-in-out ${buttonHoverBgColor} ${buttonHoverTextColor}`}>
+                <a
+                  href="https://wa.me/918129244883?text=Hi%20there!%20I'm%20interested%20in%20your%20services."
+                  target="_blank"
+                  rel="noopener noreferrer">
+                      <button className={`${buttonBgColor} ${buttonTextColor} py-2 px-6 rounded-lg font-semibold transition-all duration-300 ease-in-out ${buttonHoverBgColor} ${buttonHoverTextColor}`}>
                     Enquire Now
                 </button>
+                </a>
+              
             </div>
         </>
     )
