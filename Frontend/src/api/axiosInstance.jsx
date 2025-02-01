cdcd54a5e6566e64989c0e4b06b493604dacf425
shijiaa <shijihcl@gmail.com>
2025-02-01 15:12:48 +0530

    changes added

--- Frontend/src/api/axiosInstance.jsx
+++ Frontend/src/api/axiosInstance.jsx
@@ -1,46 +1,16 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/users",
+      baseURL:`${import.meta.env.VITE_PUBLIC_API_URL}/api/users`
 
 });
 const axiosInstanceAdmin = axios.create({
-  baseURL: "http://localhost:3000/api/admin"
+    baseURL:`${import.meta.env.VITE_PUBLIC_API_URL}/api/admin`
+  
 })
 
 
-// *********************************************************************************
-// Request interceptor Admin
-// axiosInstanceAdmin.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("adminToken");
-//     console.log(token,"interceptor page token")
-//     if (token !== null) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
 
- // Response interceptor
-// axiosInstanceAdmin.interceptors.response.use(
-//   (response) => {
-//     return response;
-//   },
-//   async (error) => {
-//     if (
-//       error.response &&
-//       error.response.data.status === 401 &&
-//       error.response.data.error === "Unauthorized - No token found"
-//     ) {
-//       console.log("Unauthorized access - no token found");
-//     }
-//     return Promise.reject(error);
-//   }
-// );
 
 
 export { axiosInstance,axiosInstanceAdmin};
