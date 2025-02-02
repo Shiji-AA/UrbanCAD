67f5539bdadea0d04f6fccf07d323d1632bcfbc5
shijiaa <shijihcl@gmail.com>
2025-02-02 16:40:09 +0530

    changed

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,26 +1,26 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server:{
-//     host: 'localhost',
-//     port: 4000
-//   }
-// })
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
+  server:{
     host: 'localhost',
-    port: 4000,
-  },
-  build: {
-    outDir: 'dist',  // Ensure the output directory is correct
-    assetsDir: 'assets',  // Ensure assets like CSS, images, etc. go into the 'assets' folder
+    port: 4000
   }
-});
+})
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: 'localhost',
+//     port: 4000,
+//   },
+//   build: {
+//     outDir: 'dist',  // Ensure the output directory is correct
+//     assetsDir: 'assets',  // Ensure assets like CSS, images, etc. go into the 'assets' folder
+//   }
+// });
