2844afd79ea920ba4dde57664248b620acf0c51f
shijiaa <shijihcl@gmail.com>
2025-02-02 17:54:06 +0530

    chages

--- Frontend/src/api/axiosInstance.jsx
+++ Frontend/src/api/axiosInstance.jsx
@@ -1,14 +1,20 @@
 import axios from "axios"
 
+// const axiosInstance = axios.create({
+//       baseURL:`${import.meta.env.VITE_PUBLIC_API_URL}/api/users`
+
+// });
+
 const axiosInstance = axios.create({
-      baseURL:`${import.meta.env.VITE_PUBLIC_API_URL}/api/users`
-
-});
-const axiosInstanceAdmin = axios.create({
-    baseURL:`${import.meta.env.VITE_PUBLIC_API_URL}/api/admin`
+    baseURL: import.meta.env.VITE_PUBLIC_API_URL + "/api/users"
+  });
+// const axiosInstanceAdmin = axios.create({
+//     baseURL:`${import.meta.env.VITE_PUBLIC_API_URL}/api/admin`
   
-})
-
+// })
+const axiosInstanceAdmin = axios.create({
+    baseURL: import.meta.env.VITE_PUBLIC_API_URL + "/api/admin"
+  });
 
 
 
