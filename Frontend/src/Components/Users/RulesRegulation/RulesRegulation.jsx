8c421d398a5364de233fff3901338a7bba1708ee
shijiaa <shijihcl@gmail.com>
2025-02-08 10:31:33 +0530

    changes added

--- Frontend/src/Components/Users/RulesRegulation/RulesRegulation.jsx
+++ Frontend/src/Components/Users/RulesRegulation/RulesRegulation.jsx
@@ -46,14 +46,14 @@ function RulesRegulation() {
 
       {/* Second Div: Requirements and Support */}
       <div className=" md:w-1/2 ">
-        <div className="rounded-lg p-12">
+        <div className="rounded-lg md:p-12 p-0">
           <h6 className="font-heading text-xl font-semibold text-aqua mb-4">
             UNLOCK THE POTENTIAL OF URBANCAD FRANCHISE
           </h6>
           <h2 className="font-heading text-4xl font-semibold text-navy mb-6">
             Step into success with an UrbanCAD franchise today
           </h2>
-          <p className="font-body text-base text-gray-600 mb-6 leading-relaxed text-lg">
+          <p className="font-body text-gray-600 mb-6 leading-relaxed text-lg">
             Joining the UrbanCAD franchise offers a unique opportunity in a
             growing industry, with proven business models and strong support. As
             a franchisee, you will receive training, marketing, and operational
